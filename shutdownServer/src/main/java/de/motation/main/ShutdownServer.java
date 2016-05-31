3b668ad3bdaef72ac2e9a7ce3b271ca5ab430c84
motation <s-o.fedders@gmx.de>
2016-05-31 23:54:31 +0200

    main class updated

--- shutdownServer/src/main/java/de/motation/main/ShutdownServer.java
+++ shutdownServer/src/main/java/de/motation/main/ShutdownServer.java
@@ -3,10 +3,12 @@ package de.motation.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by motation on 31.05.16.
  */
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan("de.motation")
 public class ShutdownServer {
