a8de62231b543fde14969b3dd6432eed552e65d4
motation <s-o.fedders@gmx.de>
2016-06-01 21:13:22 +0200

    shutdown command added

--- shutdownServer/src/main/java/de/motation/thread/CountdownThread.java
+++ shutdownServer/src/main/java/de/motation/thread/CountdownThread.java
@@ -2,6 +2,8 @@ package de.motation.thread;
 
 import de.motation.entity.Timer;
 
+import java.io.IOException;
+
 /**
  * Created by motation on 31.05.16.
  */
@@ -21,8 +23,14 @@ public class CountdownThread extends Thread {
         }
     }
 
-    private void shutdown(){
+    private void shutdown() {
         System.out.println("SHUTDOWN NOW!!!!");
+        String shutdownCmd = "shutdown -s -f";
+        try {
+            Process child = Runtime.getRuntime().exec(shutdownCmd);
+        } catch (IOException e) {
+            System.out.println("Shutdown failed");
+        }
     }
 
     public void setTimer(Timer timer) {
